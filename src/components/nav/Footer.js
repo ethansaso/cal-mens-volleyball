dc65ba8284b0732dd543cf21fd9a5680624add9f
Ethan Saso <ethansaso@gmail.com>
2024-09-03 15:20:03 -0700

    Finished responsive styling of double footer

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -6,45 +6,47 @@ const Footer = () => {
     return (
         <>
             <footer className="footer1-container">
-                <div className="footer-nav">
-                    <h4 className="footer-header">Links</h4>
-                    <div className="footer-nav-links">
-                        <div className="footer-nav-column">
-                            <p className="footer-text">
-                                <a href={`${path_prefix}`} className="footer-link" style={{color: "black"}}>Home</a>
-                            </p>
-                            <p className="footer-text">
-                                <a href={`${path_prefix}schedule`} className="footer-link" style={{color: "black"}}>Schedule</a>
-                            </p>
-                            <p className="footer-text">
-                                <a href={`${path_prefix}roster`} className="footer-link" style={{color: "black"}}>Roster</a>
-                            </p>
-                        </div>
-                        <div className="footer-nav-column">
-                            <p className="footer-text">
-                                <a href={`${path_prefix}about`} className="footer-link" style={{color: "black"}}>About</a>
-                            </p>
-                            <p className="footer-text">
-                                <a href={`${path_prefix}resources`} className="footer-link" style={{color: "black"}}>Resources</a>
-                            </p>
-                            <p className="footer-text">
-                                <a href={`${path_prefix}donate`} className="footer-link" style={{color: "black"}}>Donate</a>
-                            </p>
+                <div className="footer-content">
+                    <div className="footer-nav">
+                        <h4 className="footer-header">Links</h4>
+                        <div className="footer-nav-links">
+                            <div className="footer-nav-column">
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}`} className="footer-link" style={{color: "black"}}>Home</a>
+                                </p>
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}schedule`} className="footer-link" style={{color: "black"}}>Schedule</a>
+                                </p>
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}roster`} className="footer-link" style={{color: "black"}}>Roster</a>
+                                </p>
+                            </div>
+                            <div className="footer-nav-column">
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}about`} className="footer-link" style={{color: "black"}}>About</a>
+                                </p>
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}resources`} className="footer-link" style={{color: "black"}}>Resources</a>
+                                </p>
+                                <p className="footer-text">
+                                    <a href={`${path_prefix}donate`} className="footer-link" style={{color: "black"}}>Donate</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="footer-socials">
-                    <div>
-                        <h4 className="footer-header">Socials</h4>
-                        <div className="footer-social-links">
-                            <a href="https://www.instagram.com/calclubvolleyball/" style={{color: "black"}}>
-                                <FontAwesomeIcon icon="fa-brands fa-instagram" fixedWidth size="lg" style={{marginRight: '5px'}} />
-                                Instagram
-                            </a>
-                            <a href="https://www.facebook.com/calclubvball/" style={{color: "black"}}>
-                                <FontAwesomeIcon icon="fa-brands fa-facebook" fixedWidth size="lg" style={{marginRight: '5px'}} />
-                                Facebook
-                            </a>
+                    <div className="footer-socials">
+                        <div>
+                            <h4 className="footer-header">Socials</h4>
+                            <div className="footer-social-links">
+                                <a href="https://www.instagram.com/calclubvolleyball/" style={{color: "black"}}>
+                                    <FontAwesomeIcon icon="fa-brands fa-instagram" fixedWidth size="lg" style={{marginRight: '5px'}} />
+                                    Instagram
+                                </a>
+                                <a href="https://www.facebook.com/calclubvball/" style={{color: "black"}}>
+                                    <FontAwesomeIcon icon="fa-brands fa-facebook" fixedWidth size="lg" style={{marginRight: '5px'}} />
+                                    Facebook
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
